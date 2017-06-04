44b876894be4e33fe80ea11f32e1247660f03807
sibaram3211 <sibaram.nayak3211@gmail.com>
2017-06-05 00:07:47 +0530

    adding app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require("express");
 var jade = require("jade");
 var join = require("path").join;
@@ -21,27 +20,3 @@ app.get( "/", function(req,res){
 //app.post("/removeOrder", OrderHandler.removeOrder,  OrderHandler.errorHandling);
 
 app.listen(3006);
-=======
-var express = require("express");
-var jade = require("jade");
-var join = require("path").join;
-var bodyParser = require("body-parser");
-var app = express();
-var pubDir = join(__dirname,"/public");
-app.use( bodyParser.urlencoded( {extended:true} ) );
-app.use( bodyParser.json() );
-app.use( express.static(pubDir) );
-app.set( "view engine", "jade" );
-app.set( "views", pubDir+"/views" );
-app.get( "/", function(req,res){
-    res.render("welcome");
-});
-
-//app.get("/getOrders", OrderHandler.getOrderList,  OrderHandler.errorHandling);
-//app.get("/getPendingOrders", OrderHandler.getPendingOrders, OrderHandler.errorHandling);
-//app.post("/saveOrder", OrderHandler.saveOrder,  OrderHandler.errorHandling);
-//app.post("/updateOrder", OrderHandler.updateOrder,  OrderHandler.errorHandling);
-//app.post("/removeOrder", OrderHandler.removeOrder,  OrderHandler.errorHandling);
-
-app.listen(3006);
->>>>>>> 460e7eaa79f1774785341e24b946b9973d796c99
