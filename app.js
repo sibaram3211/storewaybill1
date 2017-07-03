0dced53f5bcf27b4551a5c9272f7b2d1c9e7d625
sibaram3211 <sibaram.nayak3211@gmail.com>
2017-07-03 21:34:18 +0530

    root directory problem fixed

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ var express = require("express");
 var jade = require("jade");
 var session = require("express-session");
 var join = require("path").join;
-var connection = require("E:/storewaybill/schema/connectionTool.js");
-var WaybillHandler = require("E:/storewaybill/schema/handlers/waybillHandler.js");
-var LoginHandler = require("E:/storewaybill/schema/handlers/loginHandler.js");
-var CheckUserAuthentication = require("E:/storewaybill/schema/handlers/checkAuthentication.js");
+var connection = require(__dirname + "/schema/connectionTool.js");
+var WaybillHandler = require(__dirname + "/schema/handlers/waybillHandler.js");
+var LoginHandler = require(__dirname + "/schema/handlers/loginHandler.js");
+var CheckUserAuthentication = require(__dirname + "/schema/handlers/checkAuthentication.js");
 var bodyParser = require("body-parser");
 var app = express();
 var pubDir = join(__dirname,"/public");
 app.use( express.static(pubDir) );
 app.set("view engine", "jade");
 app.set("views", pubDir+"/views");
-
 app.use( bodyParser.urlencoded( {extended:true} ) );
 app.use( bodyParser.json() );
-
 app.use( session({
   secret: 'MFMFMFKEY',
   resave: false,
@@ -26,8 +24,8 @@ app.use( session({
     expires: 1*60*1000
   }
 }));
-
 app.get("/waybill", CheckUserAuthentication.confirmAutho);
+//app.get("/",CheckUserAuthentication.confirmAutho);
 app.get("/login", CheckUserAuthentication.confirmAutho);
 app.post("/doLogin", LoginHandler.login);
 app.get("/getUserSession", CheckUserAuthentication.getUserSession);
